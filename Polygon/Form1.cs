0fb30dfba8a9a8be22d8d5cdb761f9e4eaa6a138
Viktor <vektor03@gmail.com>
2019-11-14 02:49:26 +0300

    Initial commit

---
+++ Polygon/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Polygon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PointF[] dots = GetDotsFromTextBox(textBox1);
+            
+            if (dots != null)
+            {
+                PolygonArea polygon = new PolygonArea(dots);
+                label2.Text = "S = " + polygon.Area.ToString();
+            }
+        }
+
+        PointF[] GetDotsFromTextBox(TextBox textBox)
+        {
+            int n = textBox.Lines.Length;
+            PointF[] dots = new PointF[n];
+
+            try
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    dots[i] = new PointF(textBox.Lines[i]);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Неправильный формат данных");
+                return null;
+            }
+
+            return dots;
+        }
+    }
+}
---
+++ Polygon/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Polygon")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Polygon")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3e4b90f8-cb1d-4bab-86b8-7211f346110e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
