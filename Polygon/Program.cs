2ecf3b741fb3095c3ef13b4043e85bdb6b31987d
Viktor <vektor03@gmail.com>
2019-11-14 04:25:37 +0300

    исправил файлы

--- Polygon/Program.cs
+++ Polygon/Program.cs
@@ -1,7 +1,6 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Polygon
@@ -9,12 +8,11 @@ namespace Polygon
     static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
